a34ba98a1ee7fcb0d4636a3e3ce5d722c90807b9
Sander Vrijders <sander.vrijders@intec.ugent.be>
2012-11-22 12:13:59 +0100

    Insert dijkstra and calculate weights

--- floodlight/src/main/java/net/floodlightcontroller/videofilter/VideoFilter.java
+++ floodlight/src/main/java/net/floodlightcontroller/videofilter/VideoFilter.java
@@ -2,7 +2,9 @@ package net.floodlightcontroller.videofilter;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
@@ -15,10 +17,15 @@ import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.core.util.AppCookie;
 import net.floodlightcontroller.counter.ICounterStoreService;
+import net.floodlightcontroller.devicemanager.IDevice;
+import net.floodlightcontroller.devicemanager.IDeviceService;
+import net.floodlightcontroller.devicemanager.SwitchPort;
 import net.floodlightcontroller.packet.Ethernet;
 import net.floodlightcontroller.packet.IPv4;
 import net.floodlightcontroller.packet.TCP;
+import net.floodlightcontroller.routing.IRoutingDecision;
 import net.floodlightcontroller.routing.Route;
 import net.floodlightcontroller.topology.ITopologyService;
 import net.floodlightcontroller.topology.NodePortTuple;
@@ -29,6 +36,7 @@ import org.openflow.protocol.OFMatch;
 import org.openflow.protocol.OFMessage;
 import org.openflow.protocol.OFPacketIn;
 import org.openflow.protocol.OFPacketOut;
+import org.openflow.protocol.OFPort;
 import org.openflow.protocol.OFType;
 import org.openflow.protocol.action.OFAction;
 import org.openflow.protocol.action.OFActionOutput;
@@ -42,10 +50,13 @@ public class VideoFilter implements IOFMessageListener, IFloodlightModule {
 	protected static Logger log;
 	protected ICounterStoreService counterStore;
 	protected OFMessageDamper messageDamper;
+	protected ITopologyService topology;
+
 	public static short FLOWMOD_DEFAULT_IDLE_TIMEOUT = 5; // in seconds
 	public static short FLOWMOD_DEFAULT_HARD_TIMEOUT = 0; // infinite
 	protected static int OFMESSAGE_DAMPER_CAPACITY = 10000; // TODO: find sweet
-															// spot
+	public static final int FORWARDING_APP_ID = 2; // TODO: This must be managed
+	// by a global APP_ID class // spot
 	protected static int OFMESSAGE_DAMPER_TIMEOUT = 250; // ms
 
 	@Override
@@ -81,6 +92,8 @@ public class VideoFilter implements IOFMessageListener, IFloodlightModule {
 	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
 		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
 		l.add(IFloodlightProviderService.class);
+		l.add(ITopologyService.class);
+		l.add(ICounterStoreService.class);
 		return l;
 	}
 
@@ -94,6 +107,8 @@ public class VideoFilter implements IOFMessageListener, IFloodlightModule {
 		log = LoggerFactory.getLogger(VideoFilter.class);
 		messageDamper = new OFMessageDamper(OFMESSAGE_DAMPER_CAPACITY,
 				EnumSet.of(OFType.FLOW_MOD), OFMESSAGE_DAMPER_TIMEOUT);
+		topology = context.getServiceImpl(ITopologyService.class);
+		counterStore = context.getServiceImpl(ICounterStoreService.class);
 	}
 
 	@Override
@@ -118,18 +133,14 @@ public class VideoFilter implements IOFMessageListener, IFloodlightModule {
 				log.info("New IPv4 message found. \n  {} \n", eth.toString());
 
 				// For debugging purposes in mininet only
-				//The method for forwarding should be put in a separate method afterwards, 
-				//since it will be repeated four times...
 				if (ippacket.getProtocol() == IPv4.PROTOCOL_ICMP) {
 
 					// ICMP icmp = (ICMP)ippacket.getPayload();
 					log.info("New ICMP message found. \n  {} \n",
 							ippacket.toString());
 
-					// Setup fields for our packets
-					OFMatch match = new OFMatch();
-					match.loadFromPacket(pi.getPacketData(), pi.getInPort());
-
+					// TODO: Call doForwardFlow with the calculated weights
+					// change doForwardFlow args...
 				}
 
 				// Get the TCP port of the packet
@@ -149,6 +160,152 @@ public class VideoFilter implements IOFMessageListener, IFloodlightModule {
 			}
 		}
 		return Command.CONTINUE;
+	}
+
+	protected void doForwardFlow(IOFSwitch sw, OFPacketIn pi,
+			FloodlightContext cntx) {
+
+		// Setup fields for our packets
+		OFMatch match = new OFMatch();
+		match.loadFromPacket(pi.getPacketData(), pi.getInPort());
+
+		// Check if we have the location of the destination
+		IDevice dstDevice = IDeviceService.fcStore.get(cntx,
+				IDeviceService.CONTEXT_DST_DEVICE);
+
+		if (dstDevice != null) {
+			IDevice srcDevice = IDeviceService.fcStore.get(cntx,
+					IDeviceService.CONTEXT_SRC_DEVICE);
+			Long srcIsland = topology.getL2DomainId(sw.getId());
+
+			if (srcDevice == null) {
+				log.debug("No device entry found for source device");
+				return;
+			}
+			if (srcIsland == null) {
+				log.debug("No openflow island found for source {}/{}",
+						sw.getStringId(), pi.getInPort());
+				return;
+			}
+
+			// Validate that we have a destination known on the same
+			// island
+			// Validate that the source and destination are not on
+			// the same switchport
+			boolean on_same_island = false;
+			boolean on_same_if = false;
+			for (SwitchPort dstDap : dstDevice.getAttachmentPoints()) {
+				long dstSwDpid = dstDap.getSwitchDPID();
+				Long dstIsland = topology.getL2DomainId(dstSwDpid);
+				if ((dstIsland != null) && dstIsland.equals(srcIsland)) {
+					on_same_island = true;
+					if ((sw.getId() == dstSwDpid)
+							&& (pi.getInPort() == dstDap.getPort())) {
+						on_same_if = true;
+					}
+					break;
+				}
+			}
+
+			if (!on_same_island) {
+				// Flood since we don't know the dst device
+				if (log.isTraceEnabled()) {
+					log.trace("No first hop island found for destination "
+							+ "device {}, Action = flooding", dstDevice);
+				}
+				doFlood(sw, pi, cntx);
+				return;
+			}
+
+			if (on_same_if) {
+				if (log.isTraceEnabled()) {
+					log.trace("Both source and destination are on the same "
+							+ "switch/port {}/{}, Action = NOP", sw.toString(),
+							pi.getInPort());
+				}
+				return;
+			}
+
+			// Install all the routes where both src and dst have
+			// attachment
+			// points. Since the lists are stored in sorted order we
+			// can
+			// traverse the attachment points in O(m+n) time
+			SwitchPort[] srcDaps = srcDevice.getAttachmentPoints();
+			Arrays.sort(srcDaps, clusterIdComparator);
+			SwitchPort[] dstDaps = dstDevice.getAttachmentPoints();
+			Arrays.sort(dstDaps, clusterIdComparator);
+
+			int iSrcDaps = 0, iDstDaps = 0;
+
+			while ((iSrcDaps < srcDaps.length) && (iDstDaps < dstDaps.length)) {
+				SwitchPort srcDap = srcDaps[iSrcDaps];
+				SwitchPort dstDap = dstDaps[iDstDaps];
+				Long srcCluster = topology
+						.getL2DomainId(srcDap.getSwitchDPID());
+				Long dstCluster = topology
+						.getL2DomainId(dstDap.getSwitchDPID());
+
+				int srcVsDest = srcCluster.compareTo(dstCluster);
+				if (srcVsDest == 0) {
+					if (!srcDap.equals(dstDap) && (srcCluster != null)
+							&& (dstCluster != null)) {
+
+						// INSERT DIJKSTRA HERE FOR OUR VIDEO FLOWS
+						// AND CALCULATE FLOWS
+						Route route;
+
+						if (route != null) {
+							if (log.isTraceEnabled()) {
+								log.trace("pushRoute match={} route={} "
+										+ "destination={}:{}", new Object[] {
+										match, route, dstDap.getSwitchDPID(),
+										dstDap.getPort() });
+							}
+							long cookie = AppCookie.makeCookie(
+									FORWARDING_APP_ID, 0);
+
+							// if there is prior routing decision
+							// use wildcard
+							Integer wildcard_hints = null;
+							IRoutingDecision decision = null;
+							if (cntx != null) {
+								decision = IRoutingDecision.rtStore.get(cntx,
+										IRoutingDecision.CONTEXT_DECISION);
+							}
+							if (decision != null) {
+								wildcard_hints = decision.getWildcards();
+							} else {
+								// L2 only wildcard if there is no
+								// prior route decision
+								wildcard_hints = ((Integer) sw
+										.getAttribute(IOFSwitch.PROP_FASTWILDCARDS))
+										.intValue()
+										& ~OFMatch.OFPFW_IN_PORT
+										& ~OFMatch.OFPFW_DL_VLAN
+										& ~OFMatch.OFPFW_DL_SRC
+										& ~OFMatch.OFPFW_DL_DST
+										& ~OFMatch.OFPFW_NW_SRC_MASK
+										& ~OFMatch.OFPFW_NW_DST_MASK;
+							}
+
+							pushRoute(route, match, wildcard_hints, pi,
+									sw.getId(), cookie, cntx, false, false,
+									OFFlowMod.OFPFC_ADD);
+						}
+					}
+					iSrcDaps++;
+					iDstDaps++;
+				} else if (srcVsDest < 0) {
+					iSrcDaps++;
+				} else {
+					iDstDaps++;
+				}
+			}
+		} else {
+			// Flood since we don't know the dst device
+			doFlood(sw, pi, cntx);
+		}
 	}
 
 	public boolean pushRoute(Route route, OFMatch match,
@@ -326,4 +483,65 @@ public class VideoFilter implements IOFMessageListener, IFloodlightModule {
 		}
 	}
 
+	protected void doFlood(IOFSwitch sw, OFPacketIn pi, FloodlightContext cntx) {
+		if (topology.isIncomingBroadcastAllowed(sw.getId(), pi.getInPort()) == false) {
+			if (log.isTraceEnabled()) {
+				log.trace(
+						"doFlood, drop broadcast packet, pi={}, "
+								+ "from a blocked port, srcSwitch=[{},{}], linkInfo={}",
+						new Object[] { pi, sw.getId(), pi.getInPort() });
+			}
+			return;
+		}
+
+		// Set Action to flood
+		OFPacketOut po = (OFPacketOut) floodlightProvider.getOFMessageFactory()
+				.getMessage(OFType.PACKET_OUT);
+		List<OFAction> actions = new ArrayList<OFAction>();
+		if (sw.hasAttribute(IOFSwitch.PROP_SUPPORTS_OFPP_FLOOD)) {
+			actions.add(new OFActionOutput(OFPort.OFPP_FLOOD.getValue(),
+					(short) 0xFFFF));
+		} else {
+			actions.add(new OFActionOutput(OFPort.OFPP_ALL.getValue(),
+					(short) 0xFFFF));
+		}
+		po.setActions(actions);
+		po.setActionsLength((short) OFActionOutput.MINIMUM_LENGTH);
+
+		// set buffer-id, in-port and packet-data based on packet-in
+		short poLength = (short) (po.getActionsLength() + OFPacketOut.MINIMUM_LENGTH);
+		po.setBufferId(pi.getBufferId());
+		po.setInPort(pi.getInPort());
+		if (pi.getBufferId() == OFPacketOut.BUFFER_ID_NONE) {
+			byte[] packetData = pi.getPacketData();
+			poLength += packetData.length;
+			po.setPacketData(packetData);
+		}
+		po.setLength(poLength);
+
+		try {
+			if (log.isTraceEnabled()) {
+				log.trace(
+						"Writing flood PacketOut switch={} packet-in={} packet-out={}",
+						new Object[] { sw, pi, po });
+			}
+			messageDamper.write(sw, po, cntx);
+		} catch (IOException e) {
+			log.error(
+					"Failure writing PacketOut switch={} packet-in={} packet-out={}",
+					new Object[] { sw, pi, po }, e);
+		}
+
+		return;
+	}
+
+	// Comparator for sorting by SwitchCluster
+	public Comparator<SwitchPort> clusterIdComparator = new Comparator<SwitchPort>() {
+		@Override
+		public int compare(SwitchPort d1, SwitchPort d2) {
+			Long d1ClusterId = topology.getL2DomainId(d1.getSwitchDPID());
+			Long d2ClusterId = topology.getL2DomainId(d2.getSwitchDPID());
+			return d1ClusterId.compareTo(d2ClusterId);
+		}
+	};
 }
