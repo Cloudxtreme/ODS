eaab2b66b79803698f2269fc7ddc0a4a71fd1426
Sander Vrijders <sander.vrijders@intec.ugent.be>
2012-12-11 10:55:56 +0100

    Constants

--- SVC-Client/src/svc/client/Constants.java
+++ SVC-Client/src/svc/client/Constants.java
@@ -11,8 +11,8 @@ package svc.client;
 public class Constants {
     //Set constants
 
-    public static final int NR_SEGMENTS = 3;
-    public static final long MILLISECONDS = 4024;
-    public static final String SERVER_LOCATION = "google.com";
+    public static final int NR_SEGMENTS = 77;
+    public static final long MILLISECONDS = 4266;
+    public static final String SERVER_LOCATION = "10.1.3.2";
     public static final int[] PORTS = {8080, 8081, 8082, 8083};
 }
